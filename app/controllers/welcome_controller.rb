a475ac1f6608633ff0205d380121e10d64e6841f
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2015-04-20 23:10:39 -0600

    authentication added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  protect_from_forgery with: :exception
+  def index
+    render :layout => 'application'
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Teacherspet::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Added on the advice of railsgirls.com when setting up devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
